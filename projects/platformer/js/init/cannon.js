b93d47992ab2cdc7c4d94015f94916435f464cb7
JacobFrederick <jacob.a.frederick12@gmail.com>
2017-10-17 00:48:47 +0000

    videogame

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,28 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary to make your level challenging.
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(450);
+        cannon.create.onBottom(300);
+        cannon.create.onLeft(350);
+        cannon.create.onRight(250);
+
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
